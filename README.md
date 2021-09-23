2fca5d9b80d80ccbe26a1c0dc3a3a58abd57ba41
nift4 <nift4@protonmail.com>
2021-09-23 16:33:56 +0200

    Initial commit
    Change-Id: I1d2d63be30d2a208b2096f4d72c0d0501ead0caa

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+git clone https://github.com/nift4/chromium.git --depth 1 -b neno_build/tools neno
+cd neno
+source envsetup.sh
+```
---
+++ envsetup.sh
@@ -0,0 +1,36 @@
+if [ "x$_NENO_ENVSETUP" == "x" ]; then
+  echo "Getting ready!"
+  git pull --rebase >/dev/null
+  if [ ! -d "$(pwd)/depot_tools" ]; then
+    git clone https://github.com/nift4/chromium.git --depth 1 -b neno_build/depot_tools depot_tools >/dev/null
+  else
+    cd depot_tools
+    git config --local advice.detachedHead false
+    git fetch origin neno_build/depot_tools --depth 1 >/dev/null
+    git checkout FETCH_HEAD >/dev/null
+    git branch -D neno_build/depot_tools >/dev/null
+    git checkout neno_build/depot_tools >/dev/null
+    cd - >/dev/null
+  fi
+  export _NENO_ENVSETUP=progress
+  source envsetup.sh
+else
+  unset _NENO_ENVSETUP
+  export NENO_ROOT="$(pwd)"
+  export PATH="$PATH:$(pwd)/depot_tools"
+  echo "To download source:"
+  echo "  fetch android"
+  echo "  gclient sync --with_branch_heads --with_tags"
+  echo "  cd src"
+  echo "  git checkout neno"
+  echo
+  echo "To update source (from src/ directory):"
+  echo "  gclient sync --with_branch_heads --with_tags"
+  echo
+  echo "To build (from src/ directory):"
+  echo "  gn gen out/neno"
+  echo "  ninja -C out/neno chrome_public_apk"
+  echo
+  echo "Enjoy!"
+  echo "~ nift4"
+fi
